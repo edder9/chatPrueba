987f1d417c553e7067aa7455ba4b32cf78c428e1
EdderFCDevGeom <edder.flores44@gmail.com>
2023-06-20 13:04:52 -0600

    ultimo intenoooddt

--- models/server.js
+++ models/server.js
@@ -13,7 +13,7 @@ class Server {
     constructor() {
 
         this.app  = express();
-        this.port = process.env.PORT || 9000;
+        this.port = process.env.PORT || 4000;
 
         // Conectar a DB
         dbConnection();
